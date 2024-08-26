0ee9bfcec2e3bf07f4063b27804ec4281d65373e
yyyyyyyyyoon <dbstj253@jbnu.ac.kr>
2024-08-26 23:20:09 +0900

    상단메뉴 경로 통일

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Shop Item v5.0.6 (https://startbootstrap.com/template/shop-item)
+* Start Bootstrap - Shop Item v5.0.6 (https://startbootstrap.com/template/로컬 스토어-item)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-로컬 스토어-item/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
